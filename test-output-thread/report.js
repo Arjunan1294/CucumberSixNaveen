7ab556b927bf79d681b10e9eeaa20c90db1d7f43
Arjunan <g.arjun94@gmail.com>
2021-06-28 08:32:37 +0530

    Cucumber first commit

--- test-output-thread/report.js
+++ test-output-thread/report.js
@@ -1,36 +1,36 @@
 $(document).ready(function() {
 CucumberHTML.timelineItems.pushArray([
   {
-    "id": "408686b8-52aa-4c63-97d2-6ad025868871",
-    "feature": "Login page feature",
-    "scenario": "Forgot Password link",
-    "start": 1611838787768,
-    "group": 15,
-    "content": "",
-    "tags": "@login,@smoke,",
-    "end": 1611838807522,
-    "className": "failed"
-  },
-  {
-    "id": "bd51b130-6ff5-4d8e-a587-8b489ce51113",
+    "id": "4f46c60c-a054-4af1-8a61-08afac8923c6",
     "feature": "Login page feature",
     "scenario": "Login with correct credentials",
-    "start": 1611838787866,
+    "start": 1624848808791,
     "group": 16,
     "content": "",
     "tags": "@login,@regression,@skip,",
-    "end": 1611838799036,
+    "end": 1624848815658,
     "className": "skipped"
   },
   {
-    "id": "ef7883d0-89a7-4753-a6f9-ddab02586e4f",
+    "id": "c7415926-ff83-4f5a-8381-67723a68970a",
+    "feature": "Login page feature",
+    "scenario": "Forgot Password link",
+    "start": 1624848808694,
+    "group": 15,
+    "content": "",
+    "tags": "@login,@smoke,",
+    "end": 1624848825791,
+    "className": "failed"
+  },
+  {
+    "id": "99aa538f-7c45-4dc9-8ef6-e3a93679d31e",
     "feature": "Login page feature",
     "scenario": "Login page title",
-    "start": 1611838787874,
+    "start": 1624848808792,
     "group": 14,
     "content": "",
     "tags": "@login,@smoke,",
-    "end": 1611838807043,
+    "end": 1624848825760,
     "className": "passed"
   }
 ]);
